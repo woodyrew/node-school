f085c040d0d5f74763c710eb5b237d6031ae7563
Andrew Goodricke <github@andrewgoodricke.com>
2013-12-23 18:40:07 +0000

    Added answer for 07

--- learnyounode/07_http-client.js
+++ learnyounode/07_http-client.js
@@ -10,3 +10,14 @@ http.get(url, function (response) {
 		console.log(chunk);
 	});
 });
+
+/* Official answer:
+
+var http = require('http')
+
+http.get(process.argv[2], function (response) {
+	response.setEncoding('utf8')
+	response.on('data', console.log)
+	response.on('error', console.error)
+})
+*/
