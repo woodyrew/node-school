b52d32498eca36537dc2d08b6e3520e075331d3c
Andrew Goodricke <github@andrewgoodricke.com>
2013-12-23 18:37:35 +0000

    Exercise 07 completed

---
+++ learnyounode/07_http-client.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var http = require('http');
+
+var url = process.argv[2];
+
+http.get(url, function (response) {
+	response.setEncoding('utf8');
+	response.on('data', function (chunk) {
+		console.log(chunk);
+	});
+});
