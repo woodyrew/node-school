193fb7bed34de2f6be27b1436903095370741413
Andrew Goodricke <github@andrewgoodricke.com>
2013-12-24 12:52:35 +0000

    Added official answer for 08

--- learnyounode/08_http-collect.js
+++ learnyounode/08_http-collect.js
@@ -9,10 +9,27 @@ http.get(url, function (response) {
 	response.setEncoding('utf8');
 	// response.on('data', function (chunk) {
 	response.pipe(bl(function (err, data) {
+		if (err) {
+			return console.error(data);
+		}
+
 		console.log(data.length);
 		console.log(data.toString());
 	}));
 });
 
 /* Official answer:
+var http = require('http')
+var bl = require('bl')
+
+http.get(process.argv[2], function (response) {
+	response.pipe(bl(function (err, data) {
+		if (err)
+			return console.error(data)
+
+		data = data.toString()
+		console.log(data.length)
+		console.log(data)
+	}))  
+})
 */
