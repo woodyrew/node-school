fc12519573510ee44ba3e1c49928210627d41225
Woody <woodyrew@users.noreply.github.com>
2014-01-07 14:48:50 +0000

    Create 03_my-first-io.js

---
+++ learnyounode/03_my-first-io.js
@@ -0,0 +1,13 @@
+"Use Strict";
+
+var fs = require('fs');
+
+var contents = fs.readFileSync(process.argv.pop());
+var lines = contents.toString().split("\n").length - 1;
+
+console.log(lines);
+
+// note you can avoid the .toString() by passing 'utf8' as the
+// second argument to readFileSync, then you'll get a String!
+//
+// fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
