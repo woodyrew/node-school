ea01e37ed274d902090e21c45e15b825425bc67a
Woody Goodricke <github@andrewgoodricke.com>
2015-04-07 13:15:43 -0300

    Added official answer

--- learnyounode/11_http-file-server.js
+++ learnyounode/11_http-file-server.js
@@ -10,3 +10,17 @@ var server = http.createServer(function (req, res) {
 	fs.createReadStream(file_to_serve).pipe(res);
 });
 server.listen(http_port)
+
+/* Official answer:
+var http = require('http')
+var fs = require('fs')
+
+var server = http.createServer(function (req, res) {
+	res.writeHead(200, { 'content-type': 'text/plain' })
+
+	fs.createReadStream(process.argv[3]).pipe(res)
+})
+
+server.listen(Number(process.argv[2]))
+
+*/
