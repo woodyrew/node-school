a3b257d6674dfb85d22e261578285000a8a3b4df
Woody Goodricke <github@andrewgoodricke.com>
2015-04-09 13:07:54 -0300

    Hapi - 09 Official Answer

--- makemehapi/09_Validation.js
+++ makemehapi/09_Validation.js
@@ -28,5 +28,31 @@ server.route({
 server.start();
 
 /* Official Answer:
-
+    var Hapi = require('hapi');
+    var Joi = require('joi');
+    
+    
+    var server = new Hapi.Server();
+    
+    server.connection({
+        host: 'localhost',
+        port: Number(process.argv[2] || 8080)
+    });
+    
+    server.route({
+        method: 'GET',
+        path: '/chickens/{breed}',
+        config: {
+            handler: function (request, reply) {
+                reply('You asked for the chicken ' + request.params.breed);
+            },
+            validate: {
+                params: {
+                    breed: Joi.string().required()
+                }
+            }
+        }
+    });
+    
+    server.start();
  */
