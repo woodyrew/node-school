44470d954b4f16367b04408acf6d1dd525333239
Woody Goodricke <github@andrewgoodricke.com>
2015-04-09 16:37:17 -0300

    Hapi - 11 Official Answer

--- makemehapi/11_Uploads.js
+++ makemehapi/11_Uploads.js
@@ -45,5 +45,44 @@ server.route({
 server.start();
 
 /* Official Answer:
+    var Hapi = require('hapi');
+    
+    var server = new Hapi.Server();
+    
+    server.connection({
+      port: Number(process.argv[2] || 8080),
+      host: 'localhost'
+    });
+    
+    server.route({
+      method: 'POST',
+      path: '/upload',
+      config: {
+        handler: function(request, reply) {
+          var body = '';
+          request.payload.file.on('data', function(data) {
+            body += data;
+          });
+          request.payload.file.on('end', function() {
+            var result = {
+              description: request.payload.description,
+              file: {
+                data: body,
+                filename: request.payload.file.hapi.filename,
+                headers: request.payload.file.hapi.headers
+              }
+            };
+            reply(JSON.stringify(result));
+          });
+        },
+        payload: {
+          output: 'stream',
+          parse: true,
+          allow: 'multipart/form-data'
+        }
+      }
+    });
+    
+    server.start();
 
  */
