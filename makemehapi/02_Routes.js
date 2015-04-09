52f3d365825f2c5bb4434548235e520cad1c71c4
Woody Goodricke <github@andrewgoodricke.com>
2015-04-09 11:37:13 -0300

    Hapi - 02 official answer

--- makemehapi/02_Routes.js
+++ makemehapi/02_Routes.js
@@ -20,3 +20,32 @@ server.route({
 });
 
 server.start();
+
+
+/* Official Answer:
+    var Hapi = require('hapi');
+    
+    
+    var server = new Hapi.Server();
+    
+    server.connection({
+        host: 'localhost',
+        port: Number(process.argv[2] || 8080)
+    });
+    
+    server.route({
+        method: 'GET',
+        path: '/{name}',
+        handler: function (request, reply) {
+            reply('Hello ' + request.params.name);
+            // a more secure alternative is this:
+            //
+            //     reply('Hello ' + encodeURIComponent(request.params.name));
+            //
+            // encodeURIComponent escapes all characters except the following: alphabetic, decimal digits, - _ . ! ~ * ' ( )
+            // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent for more details why you should call encodeURIComponent on any user-entered parameter
+        }
+    });
+    
+    server.start();
+ */
