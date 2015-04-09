389569d2c6c9021914e84f6e6097e524b50bba30
Woody Goodricke <github@andrewgoodricke.com>
2015-04-09 11:30:23 -0300

    01 - Added official answer

--- makemehapi/01_Hello_Hapi.js
+++ makemehapi/01_Hello_Hapi.js
@@ -20,3 +20,26 @@ server.connection({
 server.route({path: '/', method:'GET', handler: hello_hapi});
 
 server.start();
+
+
+/* Official Answer:
+    var Hapi = require('hapi');
+    
+    
+    var server = new Hapi.Server();
+    
+    server.connection({
+        host: 'localhost',
+        port: Number(process.argv[2] || 8080)
+    });
+    
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply('Hello Hapi');
+        }
+    });
+    
+    server.start();
+*/
