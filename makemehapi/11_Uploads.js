b7520cfdc5279d4b9974b0cc3f2c38e542b6e986
Woody Goodricke <github@andrewgoodricke.com>
2015-04-09 16:36:13 -0300

    Hapi - 11 Uploads

---
+++ makemehapi/11_Uploads.js
@@ -0,0 +1,49 @@
+var Hapi = require('hapi');
+var Joi = require('joi');
+
+var server = new Hapi.Server();
+
+server.connection({
+	host: 'localhost',
+	port: Number(process.argv[2] || 8080)
+});
+
+function upload (request, reply) {
+	var body = '';
+	request.payload.file.on('data', function (data){
+
+		body += data;
+	});
+
+	request.payload.file.on('end', function (){
+		var uploaded = {
+			description : request.payload.description,
+			file : {
+				data : body,
+				filename: request.payload.file.hapi.filename,
+				headers : request.payload.file.hapi.headers
+			}
+		};
+		reply(JSON.stringify(uploaded));
+	});
+
+}
+
+server.route({
+	method:'POST',
+	path: '/upload',
+	config: {
+		handler: upload,
+		payload: {
+			output: 'stream',
+			parse: true,
+			allow: 'multipart/form-data'
+		}
+	}
+});
+
+server.start();
+
+/* Official Answer:
+
+ */
