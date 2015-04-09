ad68484edd7dd2eadc776a1107b96f5246e17c26
Woody Goodricke <github@andrewgoodricke.com>
2015-04-09 12:09:32 -0300

    Hapi - 05 Views

---
+++ makemehapi/05_Views.js
@@ -0,0 +1,27 @@
+var Path = require('path');
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+
+server.connection({
+	host: 'localhost',
+	port: Number(process.argv[2] || 8080)
+});
+
+server.views({
+	engines: {
+		html: require('handlebars')
+	},
+	path: Path.join(__dirname, 'templates')
+});
+
+
+server.route({
+	method:'GET',
+	path: '/',
+	handler: {
+		view: "index.html"
+	}
+});
+
+server.start();
