3edd900fa96db15260ab425d4ef605f403c0acf3
Woody Goodricke <github@andrewgoodricke.com>
2015-04-09 12:14:18 -0300

    Hapi - 06 Proxies

---
+++ makemehapi/06_Proxies.js
@@ -0,0 +1,27 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+
+server.connection({
+	host: 'localhost',
+	port: Number(process.argv[2] || 8080)
+});
+
+
+
+server.route({
+	method:'GET',
+	path: '/proxy',
+	handler: {
+		proxy: {
+			host: 'localhost',
+			port: 65535
+		}
+	}
+});
+
+server.start();
+
+/* Official Answer:
+
+ */
