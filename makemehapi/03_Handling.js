f11cdd85dd1926a10af6d7bc02148cea8d5a7275
Woody Goodricke <github@andrewgoodricke.com>
2015-04-09 11:45:14 -0300

    Hapi - Official answer

--- makemehapi/03_Handling.js
+++ makemehapi/03_Handling.js
@@ -16,3 +16,26 @@ server.route({
 
 server.start();
 
+/* Official Answer:
+    var Hapi = require('hapi');
+    var Path = require('path');
+    
+    
+    var server = new Hapi.Server();
+    
+    server.connection({
+        host: 'localhost',
+        port: Number(process.argv[2] || 8080)
+    });
+    
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: {
+            file: Path.join(__dirname, '/index.html')
+        }
+    });
+    
+    server.start();
+
+*/
