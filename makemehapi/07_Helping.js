d84a45897407309aae33646aac22d67f9be86868
Woody Goodricke <github@andrewgoodricke.com>
2015-04-09 12:24:46 -0300

    Hapi - 07 Official Answer

--- makemehapi/07_Helping.js
+++ makemehapi/07_Helping.js
@@ -27,5 +27,33 @@ server.route({
 server.start();
 
 /* Official Answer:
+    var Hapi = require('hapi');
+    var Path = require('path');
+    
+    
+    var server = new Hapi.Server();
+    
+    server.connection({
+        host: 'localhost',
+        port: Number(process.argv[2] || 8080)
+    });
+    
+    server.views({
+        path: Path.join(__dirname, 'templates'),
+        engines: {
+            html: require('handlebars')
+        },
+        helpersPath:  Path.join(__dirname, 'helpers')
+    });
+    
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: {
+            view: 'template.html'
+        }
+    });
+    
+    server.start();
 
  */
