8f4d96812806751e2eeda3903bd96e622ebbb332
Woody Goodricke <github@andrewgoodricke.com>
2015-04-10 12:06:16 -0300

    Bug-Clinic - 02 Triage: Passing solution

--- bug-clinic/02_Triage.js
+++ bug-clinic/02_Triage.js
@@ -1,22 +1,27 @@
-    // scenario.js
-    var fs = require("fs");
+// scenario.js
+var fs = require("fs");
 
-    var peach = function (obj) {
-      // trace the message "traced"
-      // dump obj to stdout
-    };
+var peach = function (obj) {
+	// trace the message "traced"
+	console.trace("traced");
+	// dump obj to stdout
+	console.log(obj);
+};
 
-    var bowser = function (callback) {
-      fs.readFile(process.argv[2], {encoding : "utf8"}, callback);
-    };
+var bowser = function (callback) {
+	fs.readFile(process.argv[2], {encoding : "utf8"}, callback);
+};
 
-    var koopa = function (error, file) {
-      // handle error by printing something to stderr
+var koopa = function (error, file) {
+	// handle error by printing something to stderr
+	if (error) {
+		console.error(error);
+	}
 
-      peach(JSON.parse(file));
-    };
+	peach(JSON.parse(file));
+};
 
-    bowser(koopa);
+bowser(koopa);
 
 
 /* Official Answer:
