dfad0420a8a4e4618c99d8d52e603ade8a9f4915
Woody Goodricke <github@andrewgoodricke.com>
2015-04-10 11:39:24 -0300

    Bug Clinic - 01: Official Answer

--- bug-clinic/01_Intake.js
+++ bug-clinic/01_Intake.js
@@ -1,2 +1,8 @@
 process.stdout.write('i am okay');
 process.stderr.write('i am so incredibly not okay');
+
+/* Official Answer:
+    console.log("i am okay");
+    console.error("i am so incredibly not okay");
+
+*/
