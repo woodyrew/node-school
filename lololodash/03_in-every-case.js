c17e0e8ddcffa614945cac8373e5ecc3e93bfaa4
Woody Goodricke <github@andrewgoodricke.com>
2015-04-21 12:29:08 -0300

    lodash - 03: Official Answer

--- lololodash/03_in-every-case.js
+++ lololodash/03_in-every-case.js
@@ -16,5 +16,24 @@ module.exports = worker;
 
 
 /* Official Answer:
+    'use strict';
+    
+    var _ = require("lodash");
+    
+    module.exports = function(collection) {
+      // add a size attribute to the collection based on the item's population
+      // >= 1.0 (million) is "big"
+      // >= 0.5 && < 1.0 is "med"
+      // < 0.5 is "small"
+      return _.forEach(collection, function(item) {
+        if (item.population >= 1) {
+          item.size = "big";
+        } else if(item.population < 0.5) {
+          item.size = "small";
+        } else {
+          item.size = "med";
+        }
+      });
+    };
 
 */
