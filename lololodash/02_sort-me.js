5ba41dfe8647e7dc093b4684093e9036c4f2832c
Woody Goodricke <github@andrewgoodricke.com>
2015-04-21 12:14:21 -0300

    lodash - 02: Official Answer

--- lololodash/02_sort-me.js
+++ lololodash/02_sort-me.js
@@ -12,5 +12,20 @@ module.exports = worker;
 
 
 /* Official Answer:
+    'use strict';
+    
+    var _ = require("lodash");
+    
+    var sorting = function (collection) {
+        return _.sortBy(collection, function (item) {
+            return -item.quantity;
+        });
+    
+        /* Also possible:
+         return _.sortBy(collection,"quantity").reverse();
+         * /
+    };
+    
+    module.exports = sorting;
 
 */
